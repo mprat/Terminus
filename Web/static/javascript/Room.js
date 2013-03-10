38ed1edd0db1b08da25a31f534f01c7b6a69b536
mprat <michele.pratusevich@gmail.com>
2013-03-09 20:02:52 -0500

    cd working, including cd ..

--- Web/static/javascript/Room.js
+++ Web/static/javascript/Room.js
@@ -7,13 +7,13 @@ String.prototype.replaceAll = function(replaceThis, withThis){
 };
 
 function Room(roomname, introtext){
-	this.parents = new Array();
+	this.parents = [];
 	this.children = new Array();
 	this.items = new Array();
-	this.commands = ["cd", "ls", "less", "man", "help", "exit"];
+	this.commands = ["cd", "ls", "less", "man", "help", "exit", "pwd"];
 	this.room_name = (typeof roomname === 'undefined') ? "Generic Room": roomname;
 	this.intro_text = (typeof introtext === 'undefined') ? "This is a simple room": introtext;
-	this.cmd_text = {"man": "room man text", "less": this.room_name + " is a Room", "help": "room help text", "exit": "room exit text"};
+	this.cmd_text = {"man": "room man text", "less": this.room_name + " is a Room", "help": "room help text", "exit": "room exit text", "pwd": "You are in " + this.room_name + "."};
 };
 
 Room.prototype.toString = function(){
@@ -47,7 +47,7 @@ Room.prototype.removeChild = function(child){
 };
 
 Room.prototype.addParent = function(parent){
-	this.parents[this.parents.length] = parent;
+	this.parents[0] = parent;
 };
 
 Room.prototype.addCommand = function(cmd){
@@ -70,7 +70,27 @@ Room.prototype.ls = function(args){
 };
 
 Room.prototype.cd = function(args){
-
+	if (args.length > 1){
+		return "You can't move to multiple locations.";
+	} else if (args[0] === "..") {
+		if (this.parents.length >= 1){
+			current_room = this.parents[0];
+			return "You have moved to " + current_room.toString() + ".";
+		} else {
+			return "You are at the first room.";
+		}
+	} else if (args[0] === ".") {
+		return "You have moved to " + current_room.toString() + ".";
+	} else {
+		roomname = args[0];
+		for (var i = 0; i < this.children.length; i++){
+			if (roomname === this.children[i].toString()){
+				current_room = this.children[i];
+				return "You have moved to " + current_room.toString() + ".";
+			}
+		}
+		return "There is no room called " + args[0] + ".";
+	}
 };
 
 Room.prototype.less = function(args){
@@ -78,7 +98,7 @@ Room.prototype.less = function(args){
 };
 
 Room.prototype.man = function(args){
-	return 
+
 };
 
 Room.prototype.help = function(args){
@@ -87,4 +107,8 @@ Room.prototype.help = function(args){
 
 Room.prototype.exit = function(args){
 
+};
+
+Room.prototype.pwd = function(args){
+	return this.cmd_text["pwd"];
 };
