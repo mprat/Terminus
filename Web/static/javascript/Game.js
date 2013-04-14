8b8733513c37025c5fb796775c6c7be4bf4cba5a
Shawn Conrad <shawnrad@mit.edu>
2013-04-13 22:27:18 -0400

    tab completion should only be on appropriate things
    First argument (cd, ls, ...) determines whether we include locations
    and/or items in search for autocomplete

--- Web/static/javascript/Game.js
+++ Web/static/javascript/Game.js
@@ -95,6 +95,7 @@ $(document).ready(function() {
         if(event.keyCode == 9){
             var command = $("#term").terminal().get_command().replace(/\s+$/,"");
             var split_command = command.split(" ");
+            var first_arg = split_command[0]
             var last_arg = split_command.pop();
             //Start in a room, try to move through path, and if we get to the end
             // check whether a room/item could complete our trip
@@ -122,16 +123,31 @@ $(document).ready(function() {
                     //We've made it to the final room,
                     // so we should look for things to complete our journey
                     if(room_num == path_rooms.length-1){
+                        //IF cd, ls, cp, mv, less
                         //Compare to this room's children
-                        for(child_num = 0; child_num<search_room.children.length; child_num++){
-                            if(search_room.children[child_num].room_name.match(path_rooms[room_num])){
-                                substring_matches.push(search_room.children[child_num].room_name);
+                        if(first_arg == "cd" ||
+                            first_arg == "ls" ||
+                            first_arg == "mv" ||
+                            first_arg == "less")
+                        {
+                            for(child_num = 0; child_num<search_room.children.length; child_num++){
+                                if(search_room.children[child_num].room_name.match(path_rooms[room_num])){
+                                    substring_matches.push(search_room.children[child_num].room_name);
+                                }
                             }
                         }
+                        //IF cp, mv, less, grep, touch
                         //Compare to this room's items
-                        for(item_num = 0; item_num<search_room.items.length; item_num++){
-                            if(search_room.items[item_num].itemname.match(path_rooms[room_num])){
-                                substring_matches.push(search_room.items[item_num].itemname);
+                        if(first_arg == "cp" ||
+                            first_arg == "mv" ||
+                            first_arg == "less" ||
+                            first_arg == "grep" ||
+                            first_arg == "touch")
+                        {
+                            for(item_num = 0; item_num<search_room.items.length; item_num++){
+                                if(search_room.items[item_num].itemname.match(path_rooms[room_num])){
+                                    substring_matches.push(search_room.items[item_num].itemname);
+                                }
                             }
                         }
                         
