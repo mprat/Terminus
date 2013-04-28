a58b3cc81ba1f7287cfa891ffbea69663c2d5fd6
mprat <michele.pratusevich@gmail.com>
2013-04-28 19:57:52 -0400

    save cookie

--- Web/static/javascript/GameState.js
+++ Web/static/javascript/GameState.js
@@ -16,10 +16,10 @@ GameState.prototype.setCurrentRoom = function(newRoom){
 	date.setTime(date.getTime()+(7*24*60*60*1000));
 	console.log(document.cookie);
 	// console.log("terminuscookie="+this.getState()+"; expires="+date.toGMTString()+"; path=/");
-	// document.cookie = "terminuscookie="+this.getState()+"; expires="+date.toGMTString()+"; path=/";
+	document.cookie = "terminuscookie="+this.getState()+"; expires="+date.toGMTString()+"; path=/";
 	// console.log(document.cookie);
 	// document.cookie="thename=testcookie";
-	document.cookie="yourname=" + prompt("What is your name?");
+	// document.cookie="yourname=" + prompt("What is your name?");
 	console.log(document.cookie);
 };
 
