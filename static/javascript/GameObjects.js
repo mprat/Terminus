d62f412446446e57bad7bee5d75f6a6314128b31
mprat <michele.pratusevich@gmail.com>
2014-02-03 11:36:19 +0200

    merging with gh pages branch

--- static/javascript/GameObjects.js
+++ static/javascript/GameObjects.js
@@ -281,7 +281,7 @@ Library.addItem(new Item("PaperbackRomance",
     "You flip the paperback open to a random page. \
 \"Oh, Horatio!\" Antonia exclaimed, her bosom heaving as Horatio deftly ripped the \
 bodice from her lithe frame. Horatio gave an animalistic growl and he clasped her \
-fingers in his strong hands and brought them to rest upon his swollen— You close the \
+fingers in his strong hands and brought them to rest upon ... — You close the \
 book, disinterested, and place it back on the shelf.",
     "item_romancenovel.gif"));
 Library.addItem(new Item("HistoryOfTerminus", 
